5554aa4aeac9bf629d2dac1a39c224526c593c3a
15davidallison <15david.allison@gmail.com>
2020-11-23 11:16:07 -0800

    Add title to application window

--- src/blackboarddemonstrationui/BlackBoardDemonstrationUI.java
+++ src/blackboarddemonstrationui/BlackBoardDemonstrationUI.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -19,7 +20,7 @@ public class BlackBoardDemonstrationUI extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("UICreatedFXMLDoc.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Engine Simulation");
         stage.setScene(scene);
         stage.show();
     }
